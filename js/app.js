02cf9456eade4e4066a7b428491d98062067bdf4
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-03-05 19:28:14 +0100

    Added code to price and discount calculation

--- js/app.js
+++ js/app.js
@@ -22,31 +22,50 @@
     //1.4   Dichiarazione delle variabili.
     let name,
     surname,
-    age_groups,
+    ageGroups,
     distance
 
-    //1.5   Acquisizione bottone "Submit"
+    //1.5   Acquisizione bottone "Submit".
     const submitElement = document.getElementById('submit')//Element | null
 
-    //1.6   Acquisizione dati tramite click su buttone "Genera"
+    //1.6   Acquisizione dati tramite click su buttone "Genera".
     submitElement.addEventListener('click', function () {
     
         name = inputNameElement.value // string
         surname = inputSurnameElement.value //String
-        age_groups = inputAgeGroupsElement.value //String
+        ageGroups = inputAgeGroupsElement.value //String
         distance = inputDistanceElement.value//String
              
-        console.log(name, surname, age_groups, distance)
+        console.log('Name:' + name + ' Surname:' + surname + ' Age group:' + age_groups + ' Distance:' + distance)
         
         //2 Validazione dei dati: i campi nome e cognome non possono essere lasciati vuoti, la distanza da percorrere deve essere maggiore di 0 Km.
         
         if( 
             name != ('') &&
             surname != ('') &&
-            age_groups != ('') &&
             distance > 0 &&
             !isNaN(distance)
             ) {
+                //3 Calcolo del prezzo del biglietto.
+                    //3.1 Calcolo del prezzo pieno.
+                    const basePrice = distance * 0.21
+                    console.log('Base Price: ' + basePrice + '€')
+
+                    //3.2 Calcolo dello sconto.
+                    let discount = 0 
+                        //3.2.1 Lo sconto sarà del 20% per i minorenni e del 40% per gli over 65.
+                        if(ageGroups === "young"){
+                            discount = basePrice * 0.2
+                        } else if (ageGroups === "senior"){
+                            discount = basePrice * 0.4
+                            }
+                        console.log('Discount:' + discount + '€')
+
+                    //3.31Calcolo prezzo finale
+
+                    const finalPrice = basePrice - discount
+                    console.log('Final Price:' + finalPrice + '€')
+
         
             }   else {
                 alert('Inserire dati validi')
