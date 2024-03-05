146e2b107735e7570693e420308b484881f6102f
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-03-05 17:02:13 +0100

    Added pseudocode for data acquisition to app.css

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// TEST
+//console.log('ciuff ciuff')
+
+
+//1 Acquisizione dati.
+
+    //1.1   Acquisizione nome e cognome utente.
+        //1.1.1 Selezione dell'elemento input con ID "name".
+        //1.1.2 Recupero del valore dell'imput che verrà salvato nella variabile "name".
+
+        //1.1.3 Selezione dell'elemento input con ID "surname".
+        //1.1.4 Recupero del valore dell'imput che verrà salvato nella variabile "surname".
+
+    //1.2   Acquisizione fascia d'età utente.   
+        //1.2.1 Selezione dell'elemento input con ID "age_groups".
+        //1.2.2 Recupero del valore dell'imput che verrà salvato nella variabile "ageGroups".
+
+    //1.3   Acquisizione distanza da percorrere.
+        //1.3.1 Selezione dell'elemento input con ID "distance".
+        //1.3.2 Recupero del valore dell'imput che verrà salvato nella variabile "distance".
